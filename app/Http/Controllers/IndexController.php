7ad3ebe0318242eb8f0d11b0e45e720aca1a1283
dwijpr <dwijpr@gmail.com>
2016-05-09 01:54:58 +0700

    initial implementation

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -6,9 +6,37 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 
+use Storage;
+
 class IndexController extends Controller
 {
     public function index() {
-        return view('index');
+        $ignores = (explode("\n", Storage::get('.filesignore')));
+        $ignores[] = '.filesignore';
+        $items = [];
+        $directories = Storage::directories();
+        foreach ($directories as $key => $value) {
+            if (!in_array($value, $ignores)) {
+                $items[] = new Item($value, 'directory');
+            }
+        }
+        $files = Storage::files();
+        foreach ($files as $key => $value) {
+            if (!in_array($value, $ignores)) {
+                $items[] = new Item($value, 'file');
+            }
+        }
+        return view('index', ['items' => $items]);
+    }
+}
+
+class Item{
+    var $name, $type, $size, $modified;
+
+    public function __construct($name, $type) {
+        $this->name = $name;
+        $this->type = $type;
+        $this->size = Storage::size($name);
+        $this->modified = Storage::lastModified($name);
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,17 +1,34 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Welcome</div>
 
-                <div class="panel-body">
-                    Your Application's Landing Page.
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="table-responsive">
+                    <table class="table table-hover">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Size</th>
+                                <th>Type</th>
+                                <th>Modified</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($items as $i => $item)
+                                <tr>
+                                    <td>{{ $item->name }}</td>
+                                    <td class="text-right">{{ $item->size }}</td>
+                                    <td>{{ $item->type }}</td>
+                                    <td>{{ $item->modified }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
     </div>
-</div>
+
 @endsection
